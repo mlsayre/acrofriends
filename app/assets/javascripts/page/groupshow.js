5a2fa703f58ead20e45092893032429f727b47ac
Matt Sayre <mlsayre@gmail.com>
2014-10-08 18:22:29 -0700

    emoji start

--- app/assets/javascripts/page/groupshow.js
+++ app/assets/javascripts/page/groupshow.js
@@ -1,14 +1,20 @@
+emojify.run();
 function scrollChat() {
   $("#chatbox").animate({ scrollTop: "10000px" }, 1200);
 }
 setTimeout(scrollChat, 150);
 
 function checkChat() {
-  var pathname = window.location.pathname;
-  $("#chatboxcontent").load(pathname + " #chatboxcontent");
+  function reloadChatbox() {
+    var pathname = window.location.pathname;
+    $("#chatboxcontent").load(pathname + " #chatboxcontent");
+    emojify.run();
+  }
+  setTimeout(reloadChatbox, 150);
 
   function scrollChat() {
-    $("#chatbox").animate({ scrollTop: "10000px" }, 2000);
+    $("#chatbox").scrollTop(10000);
+    emojify.run();
   }
   setTimeout(scrollChat, 300);
 }
