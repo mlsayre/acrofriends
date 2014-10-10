cf4fc594f01e9878d3f2ad76600ab2c75d6afb91
Matt Sayre <mlsayre@gmail.com>
2014-10-10 16:42:50 -0700

    emoji in controller

--- app/assets/javascripts/page/groupshow.js
+++ app/assets/javascripts/page/groupshow.js
@@ -1,20 +1,14 @@
-emojify.run();
 function scrollChat() {
   $("#chatbox").animate({ scrollTop: "10000px" }, 1200);
 }
 setTimeout(scrollChat, 150);
 
 function checkChat() {
-  function reloadChatbox() {
-    var pathname = window.location.pathname;
-    $("#chatboxcontent").load(pathname + " #chatboxcontent");
-    emojify.run();
-  }
-  setTimeout(reloadChatbox, 150);
+  var pathname = window.location.pathname;
+  $("#chatboxcontent").load(pathname + " #chatboxcontent");
 
   function scrollChat() {
-    $("#chatbox").scrollTop(10000);
-    emojify.run();
+    $("#chatbox").animate({ scrollTop: "10000px" }, 2000);
   }
   setTimeout(scrollChat, 300);
 }
--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,4 +1,6 @@
 class ChatsController < ApplicationController
+  before_filter :emoji, :only => [ :create ]
+
   def new
     @chat = Chat.new
   end
@@ -11,6 +13,17 @@ class ChatsController < ApplicationController
       Chat.where(:group_id => params[:chat][:group_id])
         .destroy_all(['id NOT IN (?)', Chat.last(25)])
     end
+  end
+
+  protected
+
+  def emoji
+    #first strip img tags from neverdowell chatters
+    params[:chat][:message].sub!("<img", "")
+    #then see if they want to use emoji
+    allemoji = []
+    @emojipic = '<img class="emoji" src="/assets/emoji/smile.png">'
+    params[:chat][:message].sub!(":smile:", "#{ @emojipic }")
   end
 
   private
