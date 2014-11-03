211d357e8207d40b25c442b51b622b744728f045
Matt Sayre <mlsayre@gmail.com>
2014-11-02 20:04:04 -0800

    show completed games in list

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,7 @@ class PagesController < ApplicationController
   def main
     @gamesinprogressids = Gamedata.where(:user_id => current_user.id).collect(&:game_id)
     @gamesinprogress = Game.where(:id => @gamesinprogressids).where(:gameover => false).all
-    @gamescomplete = Game.where(:id => @gamesinprogressids).where(:gameover => true).all
+    @gamescomplete = Game.where(:id => @gamesinprogressids).all
     @gamesinplayround = @gamesinprogress.where('playendtime > ?', DateTime.now.utc)
       .order('playendtime ASC').all
     @gamesinvoteround = @gamesinprogress.where('playendtime <= ? AND voteendtime >= ?', DateTime.now.utc,
