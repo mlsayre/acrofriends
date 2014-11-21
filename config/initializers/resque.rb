749d43ead0f7fec424c8e0468a49c2cd6b742058
Matt Sayre <mlsayre@gmail.com>
2014-11-20 23:44:21 -0800

    resque redis config

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -23,4 +23,5 @@ Resque::Failure::Multiple.classes = [Resque::Failure::Redis]
 Resque::Failure.backend = Resque::Failure::Multiple
 Dir[File.join(Rails.root, 'app', 'jobs', '*.rb')].each { |file| require file }
 config = YAML.load(File.open("#{Rails.root}/config/resque.yml"))[Rails.env]
-Resque.redis = Redis.new(host: config['host'], port: config['port'], db: config['db'])
+#Resque.redis = Redis.new(host: config['host'], port: config['port'], db: config['db'])
+Resque.redis = ENV["REDISTOGO_URL"]
